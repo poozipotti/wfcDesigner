f6919762b37305967f41b10a51691ee5424fef7f
George Beckert <georgebeckert@gmail.com>
2024-03-20 12:52:05 -0400

    create skeletons of wfc classes

---
+++ src/wfc/WaveFunction.ts
@@ -0,0 +1,46 @@
+export class CompatibilityOracle {
+  constructor() {}
+  check() {}
+}
+export class WaveFunction {
+  static mk() {}
+  static initCoeffeceintMatric() {}
+
+  constructor() {}
+  get() {}
+  getCollapsed() {}
+  getAllCollapsed() {}
+  shannonEntropy() {}
+  isFullyCollapsed() {}
+  collapse() {}
+  constrain() {}
+}
+export class Model {
+  constructor() {}
+  run() {}
+  iterate() {}
+  propagate() {}
+  minEntroyCoOrds() {}
+}
+
+function validDirs() {}
+function parseExampleMatrix() {}
+
+const input_matrix = [
+  ["L", "L", "L", "L"],
+  ["L", "L", "L", "L"],
+  ["L", "L", "L", "L"],
+  ["L", "C", "C", "L"],
+  ["C", "S", "S", "C"],
+  ["S", "S", "S", "S"],
+  ["S", "S", "S", "S"],
+];
+const input_matrix2 = [
+  ["A", "A", "A", "A"],
+  ["A", "A", "A", "A"],
+  ["A", "A", "A", "A"],
+  ["A", "C", "C", "A"],
+  ["C", "B", "B", "C"],
+  ["C", "B", "B", "C"],
+  ["A", "C", "C", "A"],
+];
